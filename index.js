10499b67148b42565346aa82ac5da778ae0ee409
Kuday Ogunlu <kudayogunlu@gmail.com>
2022-10-02 15:47:28 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+
+let random = Math.floor(Math.random() * 10) + 1
+console.log(random)
+
+
+
+let num3 = 15
+let num4 = 11
+let mod = num3 % num4
+
+let max = 20
